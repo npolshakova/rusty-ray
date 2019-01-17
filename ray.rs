4243a05ce6c4f4ffb389dd8142d319731e693ace
npolshakova <nina_polshakova@brown.edu>
2019-01-16 21:08:48 -0500

    updates

---
+++ ray.rs
@@ -0,0 +1,68 @@
+pub struct Color {
+  pub red: f32,
+  pub green: f32,
+  pub blue: f32,
+}
+
+pub struct Sphere {
+    pub center: Point,
+    pub radius: f64,
+    pub color: Color,
+}
+
+pub struct Scene {
+    pub width: u32,
+    pub height: u32,
+    pub fov: f64,
+    pub sphere: Sphere,
+}
+
+pub struct Ray {
+    pub origin: Point,
+    pub direction: Vector3,
+}
+
+impl Ray {
+    pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
+        let fov_adjustment = (scene.fov.to_radians() / 2.0).tan();
+        let aspect_ratio = (scene.width as f64) / (scene.height as f64);
+        let sensor_x = ((((x as f64 + 0.5) / scene.width as f64) * 2.0 - 1.0) * aspect_ratio) * fov_adjustment;
+        let sensor_y = (1.0 - ((y as f64 + 0.5) / scene.height as f64) * 2.0) * fov_adjustment;
+        Ray {
+            origin: Point::zero(),
+            direction: Vector3 {
+                x: sensor_x,
+                y: sensor_y,
+                z: -1.0,
+            }
+            .normalize(),
+        }
+    }
+}
+
+pub fn render(scene: &Scene) -> DynamicImage {
+    DynamicImage::new_rgb8(scene.width, scene.height)
+}
+
+fn render_scene() {
+    let scene = Scene {
+        width: 800,
+        height: 600,
+        fov: 90.0,
+        sphere: Sphere {
+            center: Point {
+                x: 0.0,
+                y: 0.0,
+                z: -0.5,
+            },
+            radius: 1.0,
+            color: Color {
+                red: 0.4,
+                blue: 1.0,
+                green: 0.0,
+            },
+        },
+    };
+
+    let img: DynamicImage = render(&scene);
+}
